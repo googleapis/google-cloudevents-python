186c19cd472f4f44653fa02312d61c4b59e1e36b
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-23 18:39:19 -0400

    chore(revert): revert preventing normalization (#47)

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 import os
-import setuptools
-
-# Disable version normalization performed by setuptools.setup()
-try:
-    # Try the approach of using sic(), added in setuptools 46.1.0
-    from setuptools import sic
-except ImportError:
-    # Try the approach of replacing packaging.version.Version
-    sic = lambda v: v
-    try:
-        # setuptools >=39.0.0 uses packaging from setuptools.extern
-        from setuptools.extern import packaging
-    except ImportError:
-        # setuptools <39.0.0 uses packaging from pkg_resources.extern
-        from pkg_resources.extern import packaging
-    packaging.version.Version = packaging.version.LegacyVersion
+from setuptools import find_packages, setup
 
 release_status = "Development Status :: 3 - Alpha"
 
@@ -23,11 +8,9 @@ readme_filename = os.path.join(package_root, "README.md")
 with open(readme_filename, encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-version = "0.1.3"
-
-setuptools.setup(
+setup(
     name="google-events",
-    version = sic(version),
+    version = "0.1.3",
     description="A collection of first party Google Cloud Platform event objects.",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -50,7 +33,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=setuptools.find_packages("src"),
+    packages=find_packages("src"),
     package_dir={"": "src"},
     python_requires=">=3.7",
     install_requires=['python-dateutil==2.8.1'],
