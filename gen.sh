88439869871fbd83d892b656e3cf6a99e8789fc3
Dina Graves Portman <dinagraves@google.com>
2022-02-03 18:06:25 -0500

    Reverting gen.sh

--- gen.sh
+++ gen.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 
-# EVENTS_SPEC_REPO='https://github.com/googleapis/google-cloudevents'
+# # EVENTS_SPEC_REPO='https://github.com/googleapis/google-cloudevents'
 
-# echo "Cleaning things up..."
-# rm -rf workplace/ || true
-# mkdir workplace
-rm -rf src/ || true
+# # echo "Cleaning things up..."
+# # rm -rf workplace/ || true
+# # mkdir workplace
+# rm -rf src/ || true
 mkdir src
-# rm -rf gen/quicktype-wrapper || true
+rm -rf gen/quicktype-wrapper || true
 
-# echo "Cloning the event specification repository from GitHub..."
-# git clone $EVENTS_SPEC_REPO workplace/
+echo "Cloning the event specification repository from GitHub..."
+git clone $EVENTS_SPEC_REPO workplace/
 
-# echo "Adding the qt package as a dependency..."
-# mv workplace/tools/quicktype-wrapper gen/quicktype-wrapper
+echo "Adding the qt package as a dependency..."
+mv workplace/tools/quicktype-wrapper gen/quicktype-wrapper
 cd gen/quicktype-wrapper
 npm install
 cd ../
@@ -26,9 +26,9 @@ export OUT="../"
 export EXAMPLES="../workplace/testdata"
 npm run start
 
-# echo "Cleaning things up..."
-# cd ..
-# rm -rf workplace/ || true
+echo "Cleaning things up..."
+cd ..
+rm -rf workplace/ || true
 
 echo "Completed!"
 echo "To install the generated package, run pip install -e ."
