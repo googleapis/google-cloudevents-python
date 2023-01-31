1e04bf8f847d096654238cdf26721b80e7fee677
Marc Dougherty <muncus@users.noreply.github.com>
2023-01-31 15:12:11 -0800

    !feat: replace previous library version tests with new ones.
    This is in preparation for the new library, with the new generator.

---
+++ tests/util.py
@@ -0,0 +1,18 @@
+# Utility functions for writing tests for google-cloudevents
+
+import importlib
+
+
+def make_test_case(filename, pkg, cls):
+    """Returns a test function that loads the given file as the supplied proto class."""
+
+    def testfn():
+        with open(filename) as f:
+            raw_data = f.read()
+        klass = getattr(importlib.import_module(pkg), cls)
+        obj = klass.from_json(raw_data, ignore_unknown_fields=True)
+        emptyobj = klass()
+        assert obj != emptyobj
+        return obj
+
+    return testfn
